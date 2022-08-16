04dc1eebf1c5a347bcb8a32ccb688aa40abd6be7
cosmo3769 <piyushthakur3769@gmail.com>
2022-08-16 22:42:55 +0000

    added data augmentation pipeline

---
+++ ssl_study/simclrv1/pretext_task/configs/config.py
@@ -0,0 +1,35 @@
+import os
+import ml_collections
+
+
+def get_wandb_configs() -> ml_collections.ConfigDict:
+    configs = ml_collections.ConfigDict()
+    configs.project = "ssl-study"
+    configs.entity = "wandb_fc"
+
+    return configs
+
+def get_augment_configs() -> ml_collections.ConfigDict:
+    configs = ml_collections.ConfigDict()
+    configs.image_height = 224 #default - 224
+    configs.image_width = 224 #default - 224
+    configs.cropscale = (0.08, 1.0)
+    configs.cropratio = (0.75, 1.3333333333333333)
+    configs.jitterbrightness = 0.2
+    configs.jittercontrast = 0.2
+    configs.jittersaturation = 0.2
+    configs.jitterhue = 0.2
+    configs.gaussianblurlimit = (3, 7)
+    configs.gaussiansigmalimit = 0
+    configs.alwaysapply = False
+    configs.probability = 0.5
+
+    return configs
+
+def get_config() -> ml_collections.ConfigDict:
+    config = ml_collections.ConfigDict()
+    config.seed = 0
+    config.wandb_config = get_wandb_configs()
+    config.augmentation_config = get_augment_configs()
+
+    return config
---
+++ ssl_study/simclrv1/pretext_task/data/data_aug.py
@@ -0,0 +1,52 @@
+import numpy as np
+import tensorflow as tf
+from functools import partial
+import albumentations as A
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+class Augment():
+    def __init__(self, args):
+        self.args = args
+
+    def build_augmentation(self):
+        transform = A.Compose([
+            A.RandomResizedCrop(self.args.augmentation_config["img_height"], 
+                                self.args.augmentation_config["img_width"],
+                                self.args.augmentation_config["cropscale"],
+                                self.args.augmentation_config["cropratio"],
+                                self.args.augmentation_config["probability"]),
+            A.ColorJitter (self.args.augmentation_config["jitterbrightness"], 
+                           self.args.augmentation_config["jittercontrast"], 
+                           self.args.augmentation_config["jittersaturation"], 
+                           self.args.augmentation_config["hue"], 
+                           self.args.augmentation_config["alwaysapply"], 
+                           self.args.augmentation_config["probablility"]),
+            A.GaussianBlur (self.args.augmentation_config["gaussianblurlimit"], 
+                            self.args.augmentation_config["gaussiansigmalimit"], 
+                            self.args.augmentation_config["alwaysapply"], 
+                            self.args.augmentation_config["probability"])
+        ])
+        return transform
+
+    def augmentation(self, image, label):
+        aug_img = tf.numpy_function(func=self.aug_fn, inp=[image], Tout=tf.float32)
+        aug_img.set_shape((self.args.train_config["img_height"], 
+                           self.args.train_config["img_width"], 3))
+
+        aug_img = tf.image.random_flip_left_right(aug_img)
+        aug_img = tf.image.resize(aug_img, 
+                             [self.args.train_config["img_height"], 
+                             self.args.train_config["img_width"]],
+                             method='bicubic', 
+                             preserve_aspect_ratio=False)
+        aug_img = tf.clip_by_value(aug_img, 0.0, 1.0)
+        
+        return aug_img, label
+
+    def aug_fn(self, image):
+        data = {"image":image}
+        aug_data = self.transform(**data)
+        aug_img = aug_data["image"]
+
+        return aug_img.astype(np.float32)
