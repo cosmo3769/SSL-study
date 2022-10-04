24d3f4d955c2eb93810c8ddb7410db50a7c45167
cosmo3769 <piyushthakur3769@gmail.com>
2022-10-04 09:28:18 +0000

    updated dataloader for simclr(working)

--- ssl_study/simclrv1/pretext/data/__init__.py
+++ ssl_study/simclrv1/pretext/data/__init__.py
@@ -1,6 +1,5 @@
-from .data_aug import Augment
 from .dataloader import GetDataloader
 
 __all__ = [
-    'Augment', 'GetDataloader'
+    'GetDataloader'
 ]
--- ssl_study/simclrv1/pretext/data/dataloader.py
+++ ssl_study/simclrv1/pretext/data/dataloader.py
@@ -1,7 +1,8 @@
 import numpy as np
 import tensorflow as tf
-
-from .data_aug import Augment
+import numpy as np
+import tensorflow as tf
+import tensorflow_similarity as tfsim
 
 AUTOTUNE = tf.data.AUTOTUNE
 
@@ -9,6 +10,7 @@ class GetDataloader():
     def __init__(self, args):
         self.args = args
 
+  
     def get_dataloader(self, paths):
         '''
         Args:
@@ -26,10 +28,10 @@ class GetDataloader():
             .map(self.parse_data, num_parallel_calls=AUTOTUNE)
         )
 
-        # augmented view(2 views of same example in a batch)
-        augment = Augment(self.args).process(dataloader)
-        dataloader = dataloader.map(augment, num_parallel_calls=AUTOTUNE)
+        print(dataloader)
 
+        # augmented view(2 views of same example in a batch)
+        dataloader = dataloader.map(self.process, num_parallel_calls=AUTOTUNE)
 
         if self.args.bool_config["do_cache"]:
             dataloader = dataloader.cache()
@@ -44,6 +46,7 @@ class GetDataloader():
 
         return dataloader
 
+ 
     def parse_data(self, path):
         # Parse Image
         image_string = tf.io.read_file(path)
@@ -58,3 +61,68 @@ class GetDataloader():
             image = tf.clip_by_value(image, 0.0, 1.0)
 
         return image
+
+ 
+    def img_scaling(self, img):
+        return tf.keras.applications.imagenet_utils.preprocess_input(
+            img, 
+            data_format=None, 
+            mode='torch')
+
+
+    def simclr_augmenter(self, img):
+        """
+        args:
+            img: Single image tensor of shape (H, W, C)
+
+        returns:
+            A single image tensor of shape (H, W, C) with values between 0.0 and 1.0.
+        """
+
+        # random resize and crop. Increase the size before we crop.
+        img = tfsim.augmenters.augmentation_utils.cropping.crop_and_resize(
+            img, self.args.augmentation_config.image_height, self.args.augmentation_config.image_width, area_range=self.args.augmentation_config.crop_resize_area
+        )
+        
+        # The following transforms expect the data to be [0, 1]
+        # img /= 255.
+        
+        # random color jitter
+        def _jitter_transform(x):
+            return tfsim.augmenters.augmentation_utils.color_jitter.color_jitter_rand(
+                x,
+                np.random.uniform(0.0, 0.4),
+                np.random.uniform(0.0, 0.4),
+                np.random.uniform(0.0, 0.4),
+                np.random.uniform(0.0, 0.1),
+                "multiplicative",
+            )
+
+        img = tfsim.augmenters.augmentation_utils.random_apply.random_apply(_jitter_transform, p=self.args.augmentation_config.probability, x=img)
+
+        # # random grayscale
+        def _grascayle_transform(x):
+            return tfsim.augmenters.augmentation_utils.color_jitter.to_grayscale(x)
+
+        img = tfsim.augmenters.augmentation_utils.random_apply.random_apply(_grascayle_transform, p=self.args.augmentation_config.probability, x=img)
+
+        # optional random gaussian blur
+        img = tfsim.augmenters.augmentation_utils.blur.random_blur(img, self.args.augmentation_config.image_height, self.args.augmentation_config.image_width, p=self.args.augmentation_config.probability)
+
+        # random horizontal flip
+        img = tf.image.random_flip_left_right(img)
+        
+        # scale the data back to [0, 255]
+        # img = img * 255.
+        # img = tf.clip_by_value(img, 0., 255.)
+
+        return img
+
+
+    def process(self, img):
+        view1 = self.simclr_augmenter(img)
+        view1 = self.img_scaling(view1)
+        view2 = self.simclr_augmenter(img)
+        view2 = self.img_scaling(view2)
+        
+        return (view1, view2)
